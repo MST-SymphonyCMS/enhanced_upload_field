de6c9569546aa7dd4c4e09e164b10ee36dd5f8a9
Andrew Minton <moonoo.am@gmail.com>
2012-11-10 18:08:58 +0000

    Added selectbox to the upload field in section entry... now for the tricky bit.

--- fields/field.enhanced_upload.php
+++ fields/field.enhanced_upload.php
@@ -39,8 +39,8 @@
 		-------------------------------------------------------------------------*/
 
         public function displaySettingsPanel(XMLElement &$wrapper, $errors = null) {
-			parent::displaySettingsPanel($wrapper, $errors);
-
+        	parent::displaySettingsPanel($wrapper, $errors);
+			
 			// Destination Folder
 			$ignore = array(
 				'/workspace/events',
@@ -73,18 +73,17 @@
 			$this->appendRequiredCheckbox($div);
 			$this->appendShowColumnCheckbox($div);
 			$wrapper->appendChild($div);
+
         }
 
     /*-------------------------------------------------------------------------
 		Publish:
 	-------------------------------------------------------------------------*/
 
-    public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null){
+		public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null){
 			if(!is_dir(DOCROOT . $this->get('destination') . '/')){
 				$flagWithError = __('The destination directory, %s, does not exist.', array('<code>' . $this->get('destination') . '</code>'));
 			}
-
-			
 
 			elseif(!$flagWithError && !is_writable(DOCROOT . $this->get('destination') . '/')){
 				$flagWithError = __('Destination folder is not writable.') . ' ' . __('Please check permissions on %s.', array('<code>' . $this->get('destination') . '</code>'));
@@ -92,9 +91,34 @@
 
 			$label = Widget::Label($this->get('label'));
 			$label->setAttribute('class', 'file');
+
+			// Destination Folder
+			$ignore = array(
+				'/workspace/events',
+				'/workspace/data-sources',
+				'/workspace/text-formatters',
+				'/workspace/pages',
+				'/workspace/utilities'
+			);
+			$directories = General::listDirStructure(WORKSPACE, null, true, DOCROOT, $ignore);
+
+			$options = array();
+			$options[] = array($this->get('destination'), false, $this->get('destination'));
+			if(!empty($directories) && is_array($directories)){
+				foreach($directories as $d) {
+					$d = '/' . trim($d, '/');
+					if(!in_array($d, $ignore)) $options[] = array($d, ($this->get('destination') == $d), $d);
+				}
+			}
+
 			if($this->get('required') != 'yes') $label->appendChild(new XMLElement('i', __('Optional')));
+			//Allow selection of a child folder to upload the image
+			if($this->get('override') != 'no') $label->appendChild(Widget::Select('fields['.$this->get('sortorder').'][destination]', $options));
 
 			$span = new XMLElement('span', NULL, array('class' => 'frame'));
+			
+			
+			//Render the upload field or reflect the uploaded file stored in DB.
 			if($data['file']) $span->appendChild(new XMLElement('span', Widget::Anchor('/workspace' . $data['file'], URL . '/workspace' . $data['file'])));
 
 			$span->appendChild(Widget::Input('fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix, $data['file'], ($data['file'] ? 'hidden' : 'file')));
@@ -104,6 +128,21 @@
 			if($flagWithError != NULL) $wrapper->appendChild(Widget::Error($label, $flagWithError));
 			else $wrapper->appendChild($label);
     }
+
+    public function commit(){
+			if(!parent::commit()) return false;
+
+			$id = $this->get('id');
+
+			if($id === false) return false;
+
+			$fields = array();
+
+			$fields['destination'] = $this->get('override') ? '/workspace/uploads/newdirectory': $this->get('destination');
+			$fields['validator'] = ($fields['validator'] == 'custom' ? NULL : $this->get('validator'));
+
+			return FieldManager::saveSettings($id, $fields);
+		}
 		
 		
 }

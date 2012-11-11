0edf59d28a58e18ba7b5635b649b1153a8f4c82c
Andrew Minton <me@andrewminton.co.uk>
2012-11-11 23:22:37 +0000

    Beginning override of the selected upload folder... just need to capture the POST data

--- fields/field.enhanced_upload.php
+++ fields/field.enhanced_upload.php
@@ -57,11 +57,16 @@
 				'/workspace/pages',
 				'/workspace/utilities'
 			);
-			$directories = General::listDirStructure(WORKSPACE . $this->get('destination'), null, true, DOCROOT, $ignore);
-
+			
+			$output = basename($this->get('destination'));
+			
+			$directories = General::listDirStructure(WORKSPACE , null, true, DOCROOT, $ignore);
+			
+			//var_dump($directories,$output);
+			
 			$options = array();
 			$options[] = array($this->get('destination'), false, $this->get('destination'));
-			var_dump($options);die;
+			//var_dump($options);die;
 			if(!empty($directories) && is_array($directories)){
 				foreach($directories as $d) {
 					$d = '/' . trim($d, '/');
@@ -71,10 +76,11 @@
 
 			if($this->get('required') != 'yes') $label->appendChild(new XMLElement('i', __('Optional')));
 			
-			$span = new XMLElement('span', NULL, array('class' => 'frame'));
+			$span = new XMLElement('span', NULL, array('class' => 'frame enhanced_field'));
+			//$span->appendChild(new XMLElement('p', 'This'));
 			
 			//Allow selection of a child folder to upload the image
-			$choosefolder = Widget::Select('fields['.$this->get('sortorder').'][destination]', $options);
+			$choosefolder = Widget::Select('fields['.$this->get('sortorder').'][directory]', $options);
 			$choosefolder->setAttribute('class','enhanced_upload file');
 			if($this->get('override') != 'no' ) $span->appendChild($choosefolder);
 			// Add this back in when JS is figured out - && !$data['file']
@@ -92,9 +98,7 @@
     }
 
     	public function commit(){
-    	
-		//var_dump($this->get());die;
-		
+    		
 		if(!parent::commit()) return false;
 		
 			$id = $this->get('id');
@@ -106,15 +110,11 @@
 			$fields['destination'] = $this->get('destination');
 			$fields['validator'] = ($fields['validator'] == 'custom' ? NULL : $this->get('validator'));
 			$fields['override'] = $this->get('override');
-			
-			//var_dump($fields);die;
 
 			return FieldManager::saveSettings($id, $fields);
 		}
 	
 		public function prepareTableValue($data, XMLElement $link=NULL, $entry_id = null){
-			
-			//var_dump($data);
 			
 			if(!$file = $data['file']){
 				if($link) return parent::prepareTableValue(null, $link);
@@ -123,7 +123,6 @@
 
 			if($link){
 				$link->setValue(basename($file));
-				//var_dump($file);
 				return $link->generate();
 			}
 
@@ -133,6 +132,146 @@
 			}
 			
 			
+		}
+		
+		public function processRawFieldData($data, &$status, &$message=null, $simulate=false, $entry_id=NULL){
+			$status = self::__OK__;
+
+			
+			
+			//fixes bug where files are deleted, but their database entries are not.
+			if($data === NULL) {
+				return array(
+					'file' => NULL,
+					'mimetype' => NULL,
+					'size' => NULL,
+					'meta' => NULL
+				);
+			}
+			
+			
+
+			// It's not an array, so just retain the current data and return
+			if(!is_array($data)) {
+				// Ensure the file exists in the `WORKSPACE` directory
+				// @link http://symphony-cms.com/discuss/issues/view/610/
+				$file = WORKSPACE . preg_replace(array('%/+%', '%(^|/)\.\./%'), '/', $data);
+
+				$result = array(
+					'file' => $data,
+					'mimetype' => NULL,
+					'size' => NULL,
+					'meta' => NULL
+				);
+				
+				
+
+				// Grab the existing entry data to preserve the MIME type and size information
+				if(isset($entry_id) && !is_null($entry_id)) {
+					$row = Symphony::Database()->fetchRow(0, sprintf(
+						"SELECT `file`, `mimetype`, `size`, `meta` FROM `tbl_entries_data_%d` WHERE `entry_id` = %d",
+						$this->get('id'),
+						$entry_id
+					));
+
+					if(!empty($row)) $result = $row;
+				}
+
+				if(!file_exists($file) || !is_readable($file)) {
+					$message = __('The file uploaded is no longer available. Please check that it exists, and is readable.');
+					$status = self::__INVALID_FIELDS__;
+					return $result;
+				}
+				else {
+					if(empty($result['mimetype'])) $result['mimetype'] = (function_exists('mime_content_type') ? mime_content_type($file) : 'application/octet-stream');
+					if(empty($result['size'])) $result['size'] = filesize($file);
+					if(empty($result['meta'])) $result['meta'] = serialize(self::getMetaInfo($file, $result['mimetype']));
+				}
+
+				return $result;
+			}
+
+			if($simulate && is_null($entry_id)) return $data;
+
+			// Upload the new file
+			//
+			//Need to override the upload path here
+			//
+			//
+			$select = $_POST['fields']['directory'];
+			//var_dump($data,$select);die;
+			$override_path = $this->get('override') == 'yes' ? '/workspace/uploads/newdirectory' : trim($this->get('destination'));
+			$abs_path = DOCROOT . $override_path . '/';
+			$rel_path = str_replace('/workspace', '', $override_path);
+			$existing_file = NULL;
+			
+			//var_dump($abs_path,$rel_path,$existing_file,$override_path,$select);die;
+
+			if(!is_null($entry_id)) {
+				$row = Symphony::Database()->fetchRow(0, sprintf(
+					"SELECT * FROM `tbl_entries_data_%s` WHERE `entry_id` = %d LIMIT 1",
+					$this->get('id'),
+					$entry_id
+				));
+
+				$existing_file = '/' . trim($row['file'], '/');
+
+				// File was removed
+				if($data['error'] == UPLOAD_ERR_NO_FILE && !is_null($existing_file) && is_file(WORKSPACE . $existing_file)) {
+					General::deleteFile(WORKSPACE . $existing_file);
+				}
+			}
+
+			if($data['error'] == UPLOAD_ERR_NO_FILE || $data['error'] != UPLOAD_ERR_OK) {
+				return false;
+			}
+
+			// If a file already exists, then rename the file being uploaded by
+			// adding `_1` to the filename. If `_1` already exists, the logic
+			// will keep adding 1 until a filename is available (#672)
+			$new_file = $abs_path . '/' . $data['name'];
+			if(file_exists($new_file)) {
+				$i = 1;
+				$extension = General::getExtension($data['name']);
+				$renamed_file = $new_file;
+
+				do {
+					$renamed_file = General::left($new_file, -strlen($extension) - 1) . '_' . $i . '.' . $extension;
+					$i++;
+				} while (file_exists($renamed_file));
+
+				// Extract the name filename from `$renamed_file`.
+				$data['name'] = str_replace($abs_path . '/', '', $renamed_file);
+			}
+
+			// Sanitize the filename
+			$data['name'] = Lang::createFilename($data['name']);
+
+			// Actually upload the file, moving it from PHP's temporary store to the desired destination
+			if(!General::uploadFile($abs_path, $data['name'], $data['tmp_name'], Symphony::Configuration()->get('write_mode', 'file'))) {
+				$message = __('There was an error while trying to upload the file %1$s to the target directory %2$s.', array('<code>' . $data['name'] . '</code>', '<code>workspace/'.ltrim($rel_path, '/') . '</code>'));
+				$status = self::__ERROR_CUSTOM__;
+				return false;
+			}
+
+			$file = rtrim($rel_path, '/') . '/' . trim($data['name'], '/');
+
+			// File has been replaced
+			if(!is_null($existing_file) && (strtolower($existing_file) != strtolower($file)) && is_file(WORKSPACE . $existing_file)) {
+				General::deleteFile(WORKSPACE . $existing_file);
+			}
+
+			// If browser doesn't send MIME type (e.g. .flv in Safari)
+			if (strlen(trim($data['type'])) == 0) {
+				$data['type'] = (function_exists('mime_content_type') ? mime_content_type($file) : 'application/octet-stream');
+			}
+
+			return array(
+				'file' => $file,
+				'size' => $data['size'],
+				'mimetype' => $data['type'],
+				'meta' => serialize(self::getMetaInfo(WORKSPACE . $file, $data['type']))
+			);
 		}
 	
 		

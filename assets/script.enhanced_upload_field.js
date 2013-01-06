a3959fef0b5f9dee764a1ba98a32b428abd1220c
Nicolas Brassard <nico@nitriques.com>
2013-01-06 17:12:51 -0500

    Clean up js code

--- assets/script.enhanced_upload_field.js
+++ assets/script.enhanced_upload_field.js
@@ -1,21 +1,14 @@
+/*
+ * Script that adapts behavior from the inherited
+ * Upload Field behavior
+ */
 (function($) {
-	$(function(){
-		
-		// Upload fields
-		/*$('<em>' + Symphony.Language.get('Remove File') + '</em>').appendTo('label.file:has(a) span.enhanced_frame').on('click.admin', function(event) {
-			var span = $(this).parent(),
-				name = span.find('input').attr('name');
-
-			// Prevent clicktrough
-			event.preventDefault();
-			
-			//Show selectbox
-			span.find('select.enhanced_upload_select_hidden').addClass("show");
-			// Add new empty file input
-			span.find('a.enhanced_file, input.enhanced_file').remove();
-			span.prepend('<input name="' + name + '" type="file">');
-			span.find('em').remove();
-		}); */
+	
+	"use strict";
+	
+	var
+	
+	init = function(){
 		
 		// prevent clicks on the select box from opening the file dialog
 		$('.field-enhanced_upload span.frame select').on('click.admin', function (e) {
@@ -23,6 +16,8 @@
 			return false;
 		});
 	
-	});
+	};
+	
+	$(init);
 	
 })(jQuery);

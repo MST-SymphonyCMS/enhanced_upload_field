ea694ace5227d10abecab89ea806fcedcbfd80a0
Nicolas Brassard <nico@nitriques.com>
2013-01-06 18:59:34 -0500

    Added js for the remove action; Make the UI works!

--- assets/script.enhanced_upload_field.js
+++ assets/script.enhanced_upload_field.js
@@ -8,16 +8,62 @@
 	
 	var
 	
-	init = function(){
+	FIELD_SELECTOR = '.field-enhanced_upload',
+	KEY = 'enhanced-select',
+	EVENT = 'click.admin',
+	
+	_preventProp = function (e) {
+		e.stopPropagation();
+		return false;
+	},
+	
+	_hasOptions = function ($select) {
+		return $select.find('option').length > 1;
+	},
+	
+	_reCreateSelect = function (e) {
+		var 
+		select = e.data.elem.data(KEY),
+		$select = e.data.frame.append(select).find('select');
+		
+		if (!_hasOptions($select)) {
+			$select.hide();
+		}
+	},
+	
+	_eachField = function (index, elem) {
+		var 
+		$elem = $(elem),
+		$frame = $elem.find('span.frame'),
+		$select = $frame.find('select'),
+		$a = $frame.find('>span>a');
 		
 		// prevent clicks on the select box from opening the file dialog
-		$('.field-enhanced_upload span.frame select').on('click.admin', function (e) {
-			e.stopPropagation();
-			return false;
-		});
+		$select.on(EVENT, _preventProp);
+		// capture our select to be able to re-create it
+		$elem.data(KEY, $select.clone().wrap('<div>').parent().html());
+		// attach to remove event
+		$frame.on(EVENT, 'em', {elem:$elem, frame:$frame}, _reCreateSelect);
+		// if there is a file
+		if ($a.length === 1) {
+			// remove our select
+			$select.remove();
+		} 
+		// if there is no sub-directory
+		else if (!_hasOptions($select)) {
+			// hide the select
+			// do not remove it since we need its value
+			// back in the POST body.
+			$select.hide();
+		}
+	},
 	
+	init = function() {
+		// init each field
+		$('#contents').find(FIELD_SELECTOR).each(_eachField);
 	};
 	
+	// Kick off magic
 	$(init);
 	
 })(jQuery);
